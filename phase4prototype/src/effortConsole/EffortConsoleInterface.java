ef4596191294c2a6757ee7395ac3227914c84fa4
rodneyjcrum <rcrum3@asu.edu>
2023-11-19 23:10:51 -0700

    trying to do definitions

--- phase4prototype/src/effortConsole/EffortConsoleInterface.java
+++ phase4prototype/src/effortConsole/EffortConsoleInterface.java
@@ -3,6 +3,8 @@ package effortConsole;
 import javafx.application.Application;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
 import javafx.stage.Stage;
 import javafx.fxml.FXMLLoader;
  
@@ -13,9 +15,28 @@ public class EffortConsoleInterface extends Application {
     
     public void start(Stage primaryStage) {
     	try {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("consolePrototypeGUI.fxml"));
-		Parent root = loader.load();
-		Scene scene = new Scene(root);
+        //root pane will be a tab pane that alternates between tabs
+		TabPane tabPane = new TabPane();
+		
+		//load FXML file into effortConsolePane, then declare new tab, then set content of tab to the effortConsolePane
+		FXMLLoader loader = new FXMLLoader(getClass().getResource("consolePrototypeGUI.fxml"));
+		Parent effortConsolePane = loader.load();
+		Tab tab1 = new Tab();
+		tab1.setText("Effort Console");
+		tab1.setContent(effortConsolePane);
+		
+		//load FXML file into definitionsPane, then declare new tab, then set content of tab to the definitionsPane
+		FXMLLoader loader2 = new FXMLLoader(getClass().getResource("definitionsPrototypeGUI.fxml"));
+		Parent definitionsPane = loader2.load();
+		Tab tab2 = new Tab();
+		tab2.setText("Definitions");
+		tab2.setContent(definitionsPane);
+		
+		//add tabs to tabPane
+		tabPane.getTabs().addAll(tab1,tab2);
+		
+		//declare scene with tabPane, assign scene to primaryStage, show primaryStage
+		Scene scene = new Scene(tabPane);
         primaryStage.setScene(scene);
         primaryStage.show();
     	} catch(Exception e) {
